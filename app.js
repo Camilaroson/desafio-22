96cf5f600d78bdd40ade410611d3dea31df9dbbc
camila <camilaroson@hotmail.com>
2021-04-08 03:41:09 -0300

    arreglirigillos

--- app.js
+++ app.js
@@ -12,26 +12,26 @@ const producto = {
 
 app.get('/productos/vista-test',(req,res)=>{
 
-  const cant = Number(req.params.cant)
+const {cant} = req.query
 
-    //Si no existe cant,  creame 10 objetos
-  if(!cant){
-   for(let i=0 ; i<10 ; i++ ){
+  if(cant == 0){
+    res.send('No hay productos')
+}
+
+    //mostrar segun la cantidad que se indique
+        for(let i = 0; i < cant; i++){
+        productos.push(producto) 
+}
+    res.send(productos)
+
+    //Si no existe creame 10 objetos
+        if(!cant){
+        for(let i=0 ; i<10 ; i++ ){
         productos.push(producto)  
     }
     res.send(productos)
 }
 
-if(cant == 0){
-    res.send('No hay productos')
-}
-
-//mostrar segun la cantidad que se indique
-        for(let i = 0; i < cant; i++){
-        productos.push(producto) 
-
-}
-        res.send(productos)
 
 })
 
